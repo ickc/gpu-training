23903ec68b916385942893186c4b32bde276164e
Kolen Cheung <christian.kolen@gmail.com>
2025-06-18 16:24:13 +0100

    add Conways-Game-of-Life-JAX.*

---
+++ Conways-Game-of-Life-JAX.py
@@ -0,0 +1,89 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.1
+#   kernelspec:
+#     display_name: gpu-training
+#     language: python
+#     name: gpu-training
+# ---
+
+# %%
+import jax
+import jax.numpy as jnp
+import jax.scipy
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# %%
+MASK = jnp.array([
+    [True, True, True],
+    [True, False, True],
+    [True, True, True],
+])
+
+
+# %%
+@jax.jit
+def next(state):
+    n_neighbors = jax.scipy.signal.convolve(state, MASK, mode="same", precision="fastest").astype(jnp.int8)
+    return jnp.where(
+        (state & ((n_neighbors == 2) | (n_neighbors == 3))) |
+        (~state & (n_neighbors == 3)),
+        True,
+        False,
+    )
+
+
+# %% [raw]
+# N = 10
+# key = jax.random.PRNGKey(42)
+# state = jax.random.bernoulli(key, p=0.5, shape=(N, N))
+
+# %%
+state = jnp.array([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+], dtype=bool)
+
+# %%
+states = [state]
+for _ in range(10):
+    state = next(state)
+    states.append(state)
+
+# %%
+np.testing.assert_array_equal(states[0], states[3])
+np.testing.assert_array_equal(states[1], states[4])
+np.testing.assert_array_equal(states[2], states[5])
+
+# %%
+for state in states:
+    plt.imshow(state)
+    plt.show()
+
+# %%
+
+# %%
